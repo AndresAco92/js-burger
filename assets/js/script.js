5681e400fe60369cebd88b1d982a436d41332fe3
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-04-18 23:29:01 +0200

    migliorie

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,29 @@
 var price_bread = document.getElementById('price');
 var button_calc = document.getElementById('button');
 var nome_utente = document.getElementById('name');
-var ingredients = document.getElementById('ingredient-checkbox');
+var ingredients = document.getElementsByClassName('ingredient-checkbox');
 
 //configurazioni
 var price_base = 50;
 pane_lonely(price_base, price_bread);
 
 //eventi
-button_calc.addEventListener('click',function(){
+button_calc.addEventListener('click', function(){
   var check_name = nome_utente.value.trim();
 
   if(check_name.length === 0){
     alert('Inserisci Il tuo nome:')
   }else{
-    var prezzi = 0;
+    var prezzo = 0;
 
     for (var i = 0; i < ingredients.length; i++){
     var ingredienti = ingredients[i];
     
       if(ingredienti.checked === true){
-        prezzi += parseInt(ingredienti.value);
+        prezzo += parseInt(ingredienti.value);
       }
     }
-   pane_lonely = ((price_base + prezzi), price_bread);
+   pane_lonely((price_base + prezzo), price_bread);
   }
 });
 
